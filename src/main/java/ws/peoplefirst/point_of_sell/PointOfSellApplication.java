050ae98c69e455079b46244319cbdec35f1903e7
Francesco Sciadone <francescoscldn@gmail.com>
2025-02-15 02:13:30 +0100

    initial commit

---
+++ src/main/java/ws/peoplefirst/point_of_sell/PointOfSellApplication.java
@@ -0,0 +1,13 @@
+package ws.peoplefirst.point_of_sell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointOfSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointOfSellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/peoplefirst/point_of_sell/PointOfSellApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.peoplefirst.point_of_sell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointOfSellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
